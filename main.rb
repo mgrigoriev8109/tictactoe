d91819e7c5bd58d29e5cc95ac0650b08aecedfd5
Mikhail <mikhailgrigoriev92@gmail.com>
2021-08-30 11:26:29 -0400

    Organize skeletons of methods

--- main.rb
+++ main.rb
@@ -1,28 +1,42 @@
 class GameBoard
   #game_board class generates a new game_board instance with 9 O's
-  include BoardRound
+  include UpdateBoard
+  def initialize(unavailable_cells_x, available_cells_o)
+    @available_cells_o = [0..8]
+    @unavailable_cells_x = []
+  end
+
+  #include attr_accessor
+
+  def print_board
+    #printing the board is functionality that only the GameBoard class requires
+  end
 end
 
 class Player
   #first and second player generate new_round instances resetting their X selections
   include PlayerRound
+
+  def prompt_player
+    #gets the input from the player
+  end
+
+  def check_winner
+  
+  end
+  
 end
 
-module BoardRound
-  def print_board
+module UpdateBoard
+  def receive_player_input(unavailable_cells)
+    #this module method receives player answer from player instance
+  end
 
+  def update_board(player_input)
+    #this module method updates the board 
   end
 end 
 
-module PlayerRound
-  def prompt_player
-
-  end
-  def check_winner
-
-  end
-end
-
-new_board = GameBoard.new
-first_player = Player.new
-second_player = Player.new
+current_board = GameBoard.new
+player_one = Player.new
+player_two = Player.new
