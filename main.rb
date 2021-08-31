8bb42bd144848dd3c8e160d4d71ea8eb5898f6e9
Mikhail <mikhailgrigoriev92@gmail.com>
2021-08-30 22:21:16 -0400

    prompt_player gets index prompt

--- main.rb
+++ main.rb
@@ -7,7 +7,7 @@ class GameBoard
     @gameboard_array = gameboard_array
   end
 
-  def print_board(x)
+  def print_board
     p @gameboard_array[0..2]
     p @gameboard_array[3..5]
     p @gameboard_array[6..9]
@@ -19,13 +19,16 @@ class GameBoard
 end
 
 class Player
-  attr_accessor :player_selections
-
-  def initialize (player_selections)
-    @player_selections = player_selections
+  attr_accessor :player_index_choice, :player_selections_array
+  
+  def initialize (player_selections_array)
+    @player_selections_array = player_selections_array
   end
 
   def prompt_player
+    print "From 0 to 8, which box would you like to mark with an x?"
+    @player_index_choice = gets.chomp
+    p @player_index_choice
   end
 
   def check_winner
@@ -34,6 +37,8 @@ class Player
 end
 
 current_board = GameBoard.new(Array.new(9, 'o'))
-current_board.print_board(1)
+current_board.update_board(1)
+current_board.print_board
 player_one = Player.new(Array.new)
 player_two = Player.new(Array.new)
+player_one.prompt_player
