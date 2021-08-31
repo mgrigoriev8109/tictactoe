329b4b5a45a75a6b7235d3ec84b3407899e00078
Mikhail <mikhailgrigoriev92@gmail.com>
2021-08-30 21:52:56 -0400

    print_board modifies changed index to 'x'

--- main.rb
+++ main.rb
@@ -1,13 +1,24 @@
+module UpdateBoard
+  def receive_player_input(unavailable_cells)
+    #this module method receives player answer from player instance
+  end
+
+  def update_board(player_input)
+    #this module method updates the board 
+  end
+end 
+
 class GameBoard
   attr_accessor :gameboard_array
 
-  #include UpdateBoard
+  include UpdateBoard
 
   def initialize(gameboard_array)
     @gameboard_array = gameboard_array
   end
 
-  def print_board
+  def print_board(x)
+    @gameboard_array[x] = 'x'
     p gameboard_array[0..2]
     p gameboard_array[3..5]
     p gameboard_array[6..9]
@@ -15,8 +26,13 @@ class GameBoard
 end
 
 class Player
-  #first and second player generate new_round instances resetting their X selections
-  #include PlayerRound
+  attr_accessor :player_selections
+
+  include UpdateBoard
+
+  def initialize (player_selections)
+    @player_selections = player_selections
+  end
 
   def prompt_player
     #gets the input from the player
@@ -28,17 +44,7 @@ class Player
   
 end
 
-module UpdateBoard
-  def receive_player_input(unavailable_cells)
-    #this module method receives player answer from player instance
-  end
-
-  def update_board(player_input)
-    #this module method updates the board 
-  end
-end 
-
 current_board = GameBoard.new(Array.new(9, 'o'))
-current_board.print_board
-#player_one = Player.new
+current_board.print_board(1)
+player_one = Player.new(Array.new)
 #player_two = Player.new
