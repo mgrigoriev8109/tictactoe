5820b2fd5907064af1c17b526503389790ee0259
Mikhail <mikhailgrigoriev92@gmail.com>
2021-08-30 23:14:55 -0400

    Change selection to 'x' and 'o'

--- main.rb
+++ main.rb
@@ -13,8 +13,8 @@ class GameBoard
     p @gameboard_array[6..9]
   end
 
-  def update_board(y)
-    @gameboard_array[y] = 'x'
+  def update_board(index, player_selection)
+    @gameboard_array[index] = player_selection
   end
 end
 
@@ -27,34 +27,37 @@ class Player
   end
 
   def prompt_player
-    print "From 0 to 8, which box would you like to mark with an x?"
+    print "From 0 to 8, which box would you like to mark?"
     @player_index_choice = Integer(gets.chomp)
     p @player_index_choice
   end
 
   def check_winner
+    #if player_selections_array.combination(3).to_a.select{ }
   end
   
 end
 
-current_board = GameBoard.new(Array.new(9, 'o'))
+current_board = GameBoard.new(Array.new(9, '-'))
 player_one = Player.new(Array.new)
 player_two = Player.new(Array.new)
 
 loop do
 
   player_one.prompt_player
-  current_board.update_board(player_one.player_index_choice)
+  current_board.update_board(player_one.player_index_choice, 'o')
   current_board.print_board
-  unless current_board.gameboard_array.include?("o")
+  #player_one.check_winner
+  unless current_board.gameboard_array.include?("-")
     print 'The game is over, the board is full and it is a tie!'
     break
   end
 
   player_two.prompt_player
-  current_board.update_board(player_two.player_index_choice)
+  current_board.update_board(player_two.player_index_choice, 'x')
   current_board.print_board
-  unless current_board.gameboard_array.include?("o")
+  #player_two.check_winner
+  unless current_board.gameboard_array.include?("-")
     print 'The game is over, the board is full and it is a tie!'
     break
   end
